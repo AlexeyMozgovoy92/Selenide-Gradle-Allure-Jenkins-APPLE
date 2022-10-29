b8394caa3e4f4480d342191248e405336dc710b5
Mozgovoy <alex.mzg@gmail.com>
2022-10-29 15:01:18 -0400

    added remoteURL

--- src/test/java/examples/config/ProjectConfig.java
+++ src/test/java/examples/config/ProjectConfig.java
@@ -17,6 +17,7 @@ public interface ProjectConfig extends Config {
     @DefaultValue("1920x1080")
     String browserSize();
     String browserMobileView();
+   @DefaultValue("http://user1:1234@selenoid.autotests.cloud/wd/hub")
     String remoteDriverUrl();
     String videoStorage();
 }
